2afbc056c536e08198a0a60cb761c041ef7c106b
John McQueen <John.c.mcqueen-1@ou.edu>
2019-09-08 12:06:04 -0500

    feature/react-router react router initial commit

--- src/components/app.js
+++ src/components/app.js
@@ -2,12 +2,16 @@ import FlightHistory from 'components/flight-history'
 import Login from 'components/login'
 import PropTypes from 'prop-types'
 import {Provider} from 'react-redux'
+import { BrowserRouter as Router, Route} from 'react-router-dom'
 import React from 'react';
 
 function App(props) {
   return (
     <Provider store={props.store}>
-      <FlightHistory />
+      <Router>
+        <Route exact path="/" component={Login}/>
+        <Route path="/flight-history" component={FlightHistory}/>
+      </Router>
     </Provider>
   );
 }
